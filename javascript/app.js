a0e3e90128ba4714cfcaf84c13ad0c1086141b56
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-07-01 13:08:11 -0400

    Set up firebase

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAkdU7gjm_pXv1AkrKxrQ-Ff9Rfzh9w47E",
+  authDomain: "rps-multiplayer-a96a7.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-a96a7.firebaseio.com",
+  projectId: "rps-multiplayer-a96a7",
+  storageBucket: "",
+  messagingSenderId: "231341482264",
+  appId: "1:231341482264:web:450d060ef9f86b71"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
